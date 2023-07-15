40428b869a00109f19ce8eab792669a4151c7efa
Sabir222 <mrsabir4@gmail.com>
2023-07-15 02:25:49 +0100

    hope for the best

--- src/Pages/MoviePage.tsx
+++ src/Pages/MoviePage.tsx
@@ -145,7 +145,7 @@ const MoviePage: React.FC = () => {
             ) : (
               <img
                 src={`https://images.unsplash.com/photo-1604147706283-d7119b5b822c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80`}
-                className="object-cover  rounded-b-[100px]   w-full h-full "
+                className="object-cover  rounded-b-[100px] min-w-[600px]  w-full  h-[400px]"
               />
             )}
 
